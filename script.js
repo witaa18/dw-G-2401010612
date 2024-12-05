93ae914c6adacb6ef31541900cdc61b382d926a5
witaa18 <witapradnyawati83@gmail.com>
2024-12-05 05:59:47 -1200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+// Toggle class active
+const navbarNav = document.querySelector('.navbar-nav');
+// Ketika menu di klik
+document.querySelector('#menu'). onclick = () => {
+    navbarNav.classList.toggle('active');
+};
